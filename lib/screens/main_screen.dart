cf953df08df1e6fdd6e810269d1bd41d972032e0
Clément POIRET <poiret.clement@outlook.fr>
2020-07-19 22:05:25 +0200

    Built intro and main screen

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  MainScreen({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[],
+        ),
+      ),
+    );
+  }
+}
